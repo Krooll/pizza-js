07d9061b142c032f473f5e42f1e9831e37df7f6a
Pawel Krol <paullkroll@icloud.com>
2023-12-18 12:41:52 +0100

    Add using subpages booking all works

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,58 @@
-import { settings,select } from "./settings.js";
+import { settings,select, classNames } from "./settings.js";
 import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
 
   const app = {
+    initPages: function(){
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id === idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for(let link of thisApp.navLinks){
+        link.addEventListener('click', (e) => {
+          const clickedElement = e.currentTarget;
+          e.preventDefault();
+
+          /*get page id from href*/
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          /*run thisApp.activatePage with that id*/
+          thisApp.activatePage(id);
+
+          /*change URL hash*/
+          window.location.hash = '#/' + id;
+        });
+      }
+    },
+
+    activatePage: function(pageId){
+      const thisApp = this;
+
+      /* add class active to matching pages, remove from non-matching*/
+      for(let page of thisApp.pages){
+        page.classList.toggle(classNames.pages.active, page.id === pageId);
+      }
+      /* add class active to matching link, remove from non-link*/
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active, 
+          link.getAttribute('href') === '#' + pageId
+        );
+      }
+    },
+
     initMenu: function(){
       const thisApp = this;
 
@@ -50,6 +100,7 @@ import Cart from "./components/Cart.js";
       const thisApp = this;
       console.log('*** App starting ***');
       
+      thisApp.initPages();
       thisApp.initData();
       thisApp.initCart();
     },
